04aee4d60ee1ff717a837281e9e60fc1fec6c726
stacyho <stacyho@mit.edu>
2015-08-22 12:03:55 -0500

    Added comments

--- WordTree/Assets/Scripts/AudioManager.cs
+++ WordTree/Assets/Scripts/AudioManager.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Manage audio events for sound-blending
 namespace WordTree
 {
 	public class AudioManager : MonoBehaviour {
 
-		static public float clipLength = .9f;
-		public float pulseLength = .15f;
-		public float scaleUp = 1.3f;
 
+		static public float clipLength = .9f; //time allocated to play audio clip for each letter
+		public float pulseLength = .15f; //time to complete single scale up
+		public float scaleUp = 1.3f; //scale object up by this much, only for letters (not blanks)
 
+		//Play audio for each letter of word one after another, then play audio for actual word
+		//Supports words of length 3-5 letters
 		public void SpellOutWord(GameObject[] gos)
 		{
 			
@@ -20,61 +23,76 @@ namespace WordTree
 				PlaySoundAndPulseLetter (gos[3], 3);
 			if (gos.Length >= 5)
 				PlaySoundAndPulseLetter (gos[4], 4);
-			if (gos.Length >= 6)
-				PlaySoundAndPulseLetter (gos[5], 5);
 			
 			PlaySoundAndPulseWord (gos);
 			
 		}
-		
+
+		//Play audio clip for while pulsing the letter once
+		//float index: order of the letter in the word
 		void PlaySoundAndPulseLetter(GameObject go, float index)
 		{
-			
+			//Check if object has audio clip attached
 			if (go.audio != null && go.audio.clip != null) {
 				Debug.Log ("Playing clip for " + go.name);
+				//Delay playing audio clip for (index * clipLength) seconds
 				go.audio.PlayDelayed (index*clipLength);  
 			}
-			
+
+			//Wait for (index * clipLength) seconds before pulsing
 			StartCoroutine(PulseLetter (go,index*clipLength));
 		}
-		
+
+		//Play audio clip for while pulsing the word once
 		void PlaySoundAndPulseWord(GameObject[] gos)
-		{
+		{	
+			//Check if word object has audio clip attached
 			GameObject go = GameObject.FindGameObjectWithTag ("WordObject");
 			if (go.audio != null && go.audio.clip != null) {
 				Debug.Log ("Playing clip for " + go.name);
+				//Delay playing audio clip for (index * clipLength) seconds
 				go.audio.PlayDelayed ((gos.Length) * clipLength);
 			}
-			
+
+			//Wait for (gos.Length * clipLength) seconds before pulsing
 			StartCoroutine (PulseWord (gos,(gos.Length)*clipLength));
 
 		}
 
-		
+		//Pulse letter once, i.e. grow and then shrink the letter back to original size
 		IEnumerator PulseLetter(GameObject go, float delayTime)
 		{
+			//Delay the pulsing by delayTime seconds
 			yield return new WaitForSeconds (delayTime);
 
+			//if object is a letter
 			if (go.tag == "TargetLetter" || go.tag == "MovableLetter") {
 				LeanTween.scale (go, new Vector3 (scaleUp * WordCreation.letterScale, scaleUp * WordCreation.letterScale, 1), pulseLength).setDelay (.2f);
 				LeanTween.scale (go, new Vector3 (1f * WordCreation.letterScale, 1f * WordCreation.letterScale, 1), clipLength * .5f).setDelay (clipLength * .5f);
 			}
 
+			//if object is a rectangle blank
 			if (go.tag == "TargetBlank") {
-				float xScale = .7f;
-				float yScale = 1.5f;
+				//Set desired scale size of rectangle blank
+				float xScale = .7f; // horizontal scale of rectangle blank
+				float yScale = 1.5f; // vertical scale of rectangele blank
 
+				//Scale up rectangle blank by 1.15 times its original size
 				LeanTween.scale (go, new Vector3 (1.15f * xScale, 1.15f * yScale, 1), pulseLength).setDelay (.2f);
+				//Scale down rectangle blank back to original size
 				LeanTween.scale (go, new Vector3 (xScale, yScale, 1), clipLength * .5f).setDelay (clipLength * .5f);
 			}
 			Debug.Log ("Pulse on " + go.name);
 
 		}
 
+		//Pulse word once, i.e. grown and then shrink all the letters of the word at the same time
 		IEnumerator PulseWord(GameObject[] gos, float delayTime)
-		{
+		{	
+			//Delay the pulsing by delayTime seconds
 			yield return new WaitForSeconds (delayTime);
 
+			//if object is a letter
 			if (gos [0].tag == "TargetLetter" || gos [0].tag == "MovableLetter") {
 				for (int i=0; i < gos.Length; i++) {
 					LeanTween.scale (gos [i], new Vector3 (scaleUp * WordCreation.letterScale, scaleUp * WordCreation.letterScale, 1), pulseLength).setDelay(.2f);
@@ -82,12 +100,16 @@ namespace WordTree
 				}
 			}
 
+			//if object is a rectangle blank
 			if (gos [0].tag == "TargetBlank") {
-				float xScale = .7f;
-				float yScale = 1.5f;
+				//Set desired scale size of rectangle blank
+				float xScale = .7f; // horizontal scale of rectangle blank
+				float yScale = 1.5f; // vertical scale of rectangle blank
 
 				for (int i=0; i < gos.Length; i++) {
+					//Scale up rectangle blank by 1.15 times its original size
 					LeanTween.scale (gos [i], new Vector3 (1.15f * xScale, 1.15f * yScale, 1), pulseLength).setDelay (.2f);
+					//Scale down rectangle blank back to original size
 					LeanTween.scale (gos [i], new Vector3 (xScale, yScale, 1), clipLength * .5f).setDelay (clipLength * .5f);
 				}
 			}
--- WordTree/Assets/Scripts/BlankCreation.cs
+++ WordTree/Assets/Scripts/BlankCreation.cs
@@ -1,55 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Handles instantiating blanks programmatically
+//Two types of blanks: 
+// 1. Rectangle: for Spelling Game, drag letters to target rectangle blanks
+// 2. Circle: for Sound Game, drag movable circle blanks (sound bubble) to correct letter
+
 namespace WordTree{
 
 	public class BlankCreation : MonoBehaviour {
 
-		static float xScale;
-		static float yScale;
-		static float blankWidth = 2.5f;
-		static int y;
-		static int z;
+		static float xScale; // horizontal scale size of blank
+		static float yScale; // vertical scale size of blank
+		static float blankWidth = 2.5f; //distance between each blank, i.e. how spread out the blanks are
+		static int y; // y position of blank
+		static int z; // z position of blank
 
+		//Create blanks that are scrambled randomly
 		public static void CreateScrambledBlanks(string word, string[] sounds, string shape, string tag, string mode)
 		{
 
-			Color[] shuffledColors = new Color[word.Length];
-			Vector3[] posn = new Vector3[word.Length];
-			int[,] order = new int[4, word.Length];
-			
+			Color[] shuffledColors = new Color[word.Length]; //contains possible colors to be assigned to blanks
+			Vector3[] posn = new Vector3[word.Length]; //contains the position for each blank
+			int[,] order = new int[4, word.Length]; //contains pre-programmed shuffled orders, i.e. ways to rearrange the order of blanks
+
+			//Instantiate normal blanks
 			CreateBlanks (word, sounds, shape, tag, mode);
 
+			//Set 4 different shuffled orders for each possible word length
 			if (word.Length == 3){
 				order = new int[,] {{2,3,1}, {3,1,2}, {3,2,1}, {2,1,3}};
 			}
-			
 			if (word.Length == 4){
 				order = new int[,] {{2,1,4,3}, {3,4,1,2}, {4,1,3,2}, {1,3,4,2}};
 			}
-			
 			if (word.Length == 5){
 				order = new int[,] {{2,1,5,3,4}, {3,5,2,4,1}, {4,1,2,5,3}, {5,4,1,3,2}};
 			}
 
+			//Find all blanks with specified tag
 			GameObject[] blanks = GameObject.FindGameObjectsWithTag(tag);
 
+			//Set possible colors
 			Color[] colors = new Color[] {Color.green,Color.yellow,Color.cyan,Color.blue,Color.magenta};
+
+			//Shuffle colors and assign to blanks
 			shuffledColors = ShuffleArrayColor (colors);
 			for (int i=0; i<blanks.Length; i++) {
 				SpriteRenderer sprite = blanks[i].GetComponent<SpriteRenderer> ();
 				sprite.color = shuffledColors[i];
 			}
 
+			//Get position of each blank
 			for (int i=0; i<blanks.Length; i++)
 				posn [i] = blanks[i].transform.position;
+
+			//int index: for determining which shuffled order to use (4 to choose from currently)
 			int index = Random.Range (0,4);
+			//Reassign positions of each blank, according to shuffled order chosen
 			for (int i = 0; i<blanks.Length; i++) {
 				blanks [i].transform.position = posn [order[index, i]-1];
 			}
 			
 		}
 
+		//For shuffling a Vector3[] array
 		static Vector3[] ShuffleArray(Vector3[] array)
 		{
 			for (int i = array.Length; i > 0; i--)
@@ -62,6 +77,7 @@ namespace WordTree{
 			return array;
 		}
 
+		//For shuffling a Color[] array
 		static Color[] ShuffleArrayColor(Color[] array)
 		{
 			for (int i = array.Length; i > 0; i--)
@@ -74,39 +90,43 @@ namespace WordTree{
 			return array;
 		}
 
+		//Instantiate blanks. Takes in the desired word, phonemes, blank shape (Rectangle or Circle), tag (MovableBlank or TargetBlank),
+		// and game mode (SpellingGame or SoundGame).
 		public static void CreateBlanks(string word, string[] sounds, string shape, string tag, string mode)
 		{
+			//Set scale size
 			if (shape == "Rectangle") {
 				xScale = .7f;
 				yScale = 1.5f;
 			}
-
 			if (shape == "Circle") {
 				xScale = .3f;
 				yScale = .3f;
 			}
 
-
+			//Set y position
 			if (mode == "SpellingGame")
 				y = -3;
 			if (mode == "SoundGame")
 				y = 0;
 
-
+			//Set z position
 			if (tag == "MovableBlank")
-				z = -2;
+				z = -2; //movable objects set at z = -2
 			if (tag == "TargetBlank")
-				z = 0;
+				z = 0; //static objects set at z = 0
 
 
-			Vector3[] position = new Vector3[word.Length];
-			
+			//Create array of uppercase letters
 			string[] letterArray = new string[word.Length];
 			for (int i=0; i<word.Length; i++) {
 				char letter = System.Char.ToUpper (word [i]);
 				letterArray [i] = System.Char.ToString (letter);
 			}
-			
+
+			//Set x positions of each letter, for each possible word length
+			//Word is centered, letters evenly spaced out according to blankWidth
+			Vector3[] position = new Vector3[word.Length];
 			if (word.Length == 3) {
 				position = new Vector3[3]{
 					new Vector3 (-1f * blankWidth, y, z),
@@ -114,7 +134,6 @@ namespace WordTree{
 					new Vector3 (1f * blankWidth, y, z)
 				};
 			}
-			
 			if (word.Length == 4) {
 				position = new Vector3[4] {
 					new Vector3 (-1.5f * blankWidth, y, z),
@@ -123,7 +142,6 @@ namespace WordTree{
 					new Vector3 (1.5f * blankWidth, y, z),
 				};
 			}
-			
 			if (word.Length == 5) {
 				position = new Vector3[5] {
 					new Vector3 (-2f * blankWidth, y, z),
@@ -134,6 +152,7 @@ namespace WordTree{
 				};
 			}
 
+			//Assign properties according to input given, then instantiate each blank
 			for (int i=0; i<word.Length; i++) {
 				ObjectProperties blank = ObjectProperties.CreateInstance (letterArray [i], tag, position [i], new Vector3 (xScale, yScale, 1), shape, "Phonemes/" + sounds [i]);
 				ObjectProperties.InstantiateObject (blank);
