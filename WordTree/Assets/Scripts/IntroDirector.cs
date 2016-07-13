cab8153e9c4d7a0c8afd466ebdd937b32caad2cf
Michal Shlapentokh-Rothman <michalshlapentokh-rothman@dhcp-18-189-9-121.dyn.MIT.EDU>
2016-07-13 14:38:53 -0400

    Add Utilities File
    -Add Utilities file to keep code for camera set up
    -Reference Utility file for function to set up camera in director scripts

--- WordTree/Assets/Scripts/IntroDirector.cs
+++ WordTree/Assets/Scripts/IntroDirector.cs
@@ -10,46 +10,8 @@ namespace WordTree
 
 		// Called on start, used to initialize stuff
 		void Start () {
-			//set up camera view to reflect screen size
-
-			// game is designed for 16.0:9.0 aspect ratio
-			float targetaspect = 16.0f / 9.0f;
-
-			// determine the game window's current aspect ratio
-			float windowaspect = (float)Screen.width / (float)Screen.height;
-
-			//want game's current aspect ratio to match the target aspect ratio
-			// current viewport height should be scaled by this amount
-			float scaleHeight = windowaspect / targetaspect;
-
-			// if scaleHeight is less than current height, create rectangle to so desired height:width ratio 
-			//is created
-			if (scaleHeight < 1.0f)
-			{
-				Rect rect = Camera.main.rect;
-
-				rect.width = 1.0f;
-				rect.height = scaleHeight;
-				rect.x = 0;
-				rect.y = (1.0f - scaleHeight) / 2.0f;
-
-				Camera.main.rect = rect;
-			}
-			//if scaleHeight is greater or equal to current height, create rectangle so 
-			//desired height:width ratio is created
-			else 
-			{
-				float scalewidth = 1.0f / scaleHeight;
-
-				Rect rect = Camera.main.rect;
-
-				rect.width = scalewidth;
-				rect.height = 1.0f;
-				rect.x = (1.0f - scalewidth) / 2.0f;
-				rect.y = 0;
-
-				Camera.main.rect = rect;
-			}
+			//Scale graphics to screen size
+			Utilities.setCameraViewForScreen (); 
 			//create instance of grestureManager
 			GestureManager gestureManager =GameObject.FindGameObjectWithTag(Constants.Tags.TAG_GESTURE_MANAGER)
 				.GetComponent<GestureManager>();
